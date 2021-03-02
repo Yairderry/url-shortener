b564e913daca3425c07261b948d467f00124dff1
Yairderry <77237113+Yairderry@users.noreply.github.com>
2021-03-02 22:21:13 +0200

    moved classes to a separate file

--- api/shorturl/index.js
+++ api/shorturl/index.js
@@ -1,69 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const axios = require("axios").default;
-const { urlCheck, dateToSqlFormat } = require("../../utils");
-
-class DataBase {
-  constructor() {
-    this.urls = [];
-  }
-
-  getData(url) {
-    axios.get(url).then((res) => {
-      res.data.record.urls.forEach(
-        ({ originalUrl, redirectCount, shortUrlId, creationDate }) => {
-          console.log({ originalUrl, redirectCount, shortUrlId, creationDate });
-          this.urls.push(
-            new Url(originalUrl, creationDate, shortUrlId, redirectCount)
-          );
-        }
-      );
-    });
-  }
-
-  setData(url) {
-    axios.put(url, this).then((res) => {
-      console.log(res);
-    });
-  }
-
-  addUrl(
-    originalUrl,
-    date = new Date(),
-    shortUrlId = this.urls.length,
-    redirectCount = 0
-  ) {
-    const url = new Url(
-      originalUrl,
-      dateToSqlFormat(date),
-      shortUrlId,
-      redirectCount
-    );
-    this.urls.push(url);
-    this.setData(process.env.DB_URL);
-  }
-
-  findUrl(originalUrl, shortUrlId) {
-    const urlByOriginalUrl = this.urls.filter(
-      (url) => url.originalUrl === originalUrl
-    )[0];
-    const urlByShortUrlId = this.urls.filter(
-      (url) => url.shortUrlId.toString() === shortUrlId
-    )[0];
-
-    if (urlByOriginalUrl) return urlByOriginalUrl;
-    if (urlByShortUrlId) return urlByShortUrlId;
-  }
-}
-
-class Url {
-  constructor(originalUrl, creationDate, shortUrlId, redirectCount = 0) {
-    this.originalUrl = originalUrl;
-    this.creationDate = creationDate;
-    this.shortUrlId = shortUrlId;
-    this.redirectCount = redirectCount;
-  }
-}
+const DataBase = require("../../classes.js");
+const { urlCheck } = require("../../utils");
 
 /*------------------------------------------------------------------*/
 const database = new DataBase();
@@ -77,7 +15,6 @@ shortUrl.use("/public", express.static(`./public`));
 
 shortUrl.post("/new", urlCheck, (req, res) => {
   const { url } = req.body;
-  console.log(req.body);
   const theUrl = database.findUrl(url);
 
   if (!theUrl) {
@@ -98,7 +35,6 @@ shortUrl.post("/new", urlCheck, (req, res) => {
 shortUrl.get("/:id", (req, res) => {
   const { id } = req.params;
 
-  console.log(id);
   const url = database.findUrl(null, id);
   url.redirectCount++;
   database.setData(process.env.DB_URL);
